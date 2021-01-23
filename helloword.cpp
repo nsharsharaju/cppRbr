df38513e18bb365dd9b21a22ffa6b95473364768
Sri Harsha Nadimpalli <harsha.sri@investwellonline.com>
2021-01-23 07:24:02 +0530

    A some initial c++ programs Signed-off-by: Sri Harsha Nadimpalli <harsha.sri@investwellonline.com>

---
+++ helloword.cpp
@@ -0,0 +1,17 @@
+/*
+Multile line
+Comment*/
+
+#include<iostream>
+
+//Sinle line comment
+using namespace std;
+
+//This is where the execution of program begins
+int main()
+{
+    // displays Hellow World! on Screen
+    cout<<"Hellow World!";
+
+    return 0;
+}
