182389b321ffb4baab03b3ba9c0483629ff72044
Sri Harsha Nadimpalli <harsha.sri@investwellonline.com>
2021-02-10 08:40:02 +0530

    Add ofElse statement Signed-off-by: Sri Harsha Nadimpalli <harsha.sri@investwellonline.com>

---
+++ ifElse.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+    if(n == 0) {
+        cout<<"If executes"<<endl;
+    }
+    else{
+        cout<<"I don't think if is right :P"<<endl;
+    }
+    cout<<"I execute anyway!"<<endl;
+    return 0;
+}
