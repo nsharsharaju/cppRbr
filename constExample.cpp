e52c5f0ee4e9ff6c1ad1d867f9741862a16379cc
Sri Harsha Nadimpalli <harsha.sri@investwellonline.com>
2021-02-10 08:22:32 +0530

    Add const example Signed-off-by: Sri Harsha Nadimpalli <harsha.sri@investwellonline.com>

---
+++ constExample.cpp
@@ -0,0 +1,11 @@
+#include<string>
+#include<iostream>
+
+using namespace std;
+
+int main () {
+    const int a = 10;
+    const int b = a + 1;
+    // a = a+1; This code will give error 
+    return 0;
+}
