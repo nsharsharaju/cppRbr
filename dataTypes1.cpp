7fa24ff4dfb9b573a804263a056bb6026b82a67a
Sri Harsha Nadimpalli <harsha.sri@investwellonline.com>
2021-02-10 07:02:26 +0530

    Add dataType functions Signed-off-by: Sri Harsha Nadimpalli <harsha.sri@investwellonline.com>

---
+++ dataTypes1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+using namespace std;
+
+int main() 
+{
+    int a = 'x';
+    char b = 'x';
+    cout << a << endl;
+    cout << b << endl;
+    return 1;
+}
---
+++ dataTypes2.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    char c = 'y';
+    cout << (int)c << endl;
+    cout << c << endl;
+    return 1;
+}
