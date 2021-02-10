bd433358abaeccc98bf9ff11deb7f371fca0c264
Sri Harsha Nadimpalli <harsha.sri@investwellonline.com>
2021-02-10 08:53:48 +0530

    Add doWhile loop Signed-off-by: Sri Harsha Nadimpalli <harsha.sri@investwellonline.com>

---
+++ doWhileLoop.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    int i = 1;
+    do {
+        cout<<i<<endl;
+        i = i + 1;
+    } while(i<=n);
+    return 0;
+}
