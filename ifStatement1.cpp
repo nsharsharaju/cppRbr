099f47614172e2f151a8bf4a5b0b69f4fda8ecc4
Sri Harsha Nadimpalli <harsha.sri@investwellonline.com>
2021-02-10 08:35:28 +0530

    Add if statement Signed-off-by: Sri Harsha Nadimpalli <harsha.sri@investwellonline.com>

---
+++ ifStatement1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    if(n == 0){
+        cout << "If executes"<<endl;
+    }
+    cout<<"I execute anyway!"<<endl;
+    return 0;
+}
