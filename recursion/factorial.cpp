35390f67a2f26615ac990f862424e7473e3705aa
Sri Harsha Nadimpalli <harsha.sri@investwellonline.com>
2021-02-14 19:10:38 +0530

    Add recursion 1st commmit Signed-off-by: Sri Harsha Nadimpalli <harsha.sri@investwellonline.com>

---
+++ recursion/factorial.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int factorial(int n){
+    if(n<2)
+        return 1;
+    else
+        return n*factorial(n-1);
+}
+
+int main(){
+    int n;
+    cin>>n;
+    cout<<factorial(n)<<endl;
+    return 0;
+}
