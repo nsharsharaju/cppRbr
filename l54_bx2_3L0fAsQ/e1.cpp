77a8e4cd64d4efd91fff2c5f279b455965a1b00a
Sri Harsha Nadimpalli <harsha.sri@investwellonline.com>
2021-06-17 20:39:51 +0530

    Add more functions on max and min functions Signed-off-by: Sri Harsha Nadimpalli <harsha.sri@investwellonline.com>

---
+++ l54_bx2_3L0fAsQ/e1.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<bits/stdtr1c++.h>
+using namespace std;
+
+int main() {
+    int a = 5, b = 10;
+    cout<<max(a,b)<<endl;
+    cout<<min(a,b)<<endl;
+
+    char a1 = 'a', b1 = 'x';
+    cout<<max(a1,b1)<<endl;
+    cout<<min(a1,b1)<<endl;
+
+    string a2 = "Jay", b2 = "Bansal";
+    cout<<max(a2,b2)<<endl;
+    cout<<min(a2,b2)<<endl;
+    return 0;
+}
