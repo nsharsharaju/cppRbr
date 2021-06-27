5506f1fa81de0e4204859f3c20b65c15b8a806eb
Sri Harsha Nadimpalli <harsha.sri@investwellonline.com>
2021-06-27 10:06:55 +0530

    Add a programs on pairs Signed-off-by: Sri Harsha Nadimpalli <harsha.sri@investwellonline.com>

---
+++ l67_Un_zAJZ32AI/e1.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdtr1c++.h>
+using namespace std;
+
+int main(){
+    pair<int,char> p1;
+
+    p1.first = 1;
+    p1.second = 'J';
+
+    cout << p1.first << endl;
+    cout << p1.second << endl;
+
+    return 0;
+}
