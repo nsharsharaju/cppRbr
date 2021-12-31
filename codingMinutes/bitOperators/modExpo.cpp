f9b8ccb6228779578eadc63747bb7a78de2991a7
Sri Harsha Nadimpalli <harsha.sri@investwellonline.com>
2021-12-31 10:17:47 +0530

    Add mod expo Signed-off-by: Sri Harsha Nadimpalli <harsha.sri@investwellonline.com>

--- codingMinutes/bitOperators/modExpo.cpp
+++ codingMinutes/bitOperators/modExpo.cpp
@@ -1,20 +1,26 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main() {
-    int x, y, p;
+int power(int x, int y, int mod) {
     int res = 1;
-    cin >> x >> y >> p;
-    x = x % p;
+    x = x % mod;
     if(x == 0 ) return 0;
     while(y>0) {
         int lastBit = (y&1);
         if(lastBit) {
-            res = (res*x)%p;
+            res = (res*x)%mod;
         }
         y = y >> 1;
-        x = (x*x) % p;
+        x = (x*x) % mod;
     }
-    cout << res << endl;
+    return res;
+}
+
+int main() {
+    int x,y,mod;
+    cin >> x >> y >> mod;
+    int ans = power(x,y,mod);
+    cout << ans << endl;
     return 0;
 }
+
