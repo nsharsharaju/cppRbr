f0caab270e8e80d60068e24dbf64942faa7009b5
Sri Harsha Nadimpalli <harsha.sri@investwellonline.com>
2021-12-31 10:58:46 +0530

    Add factorial Signed-off-by: Sri Harsha Nadimpalli <harsha.sri@investwellonline.com>

---
+++ codingMinutes/recursion/factorial.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int factorial(int n) {
+    if(n == 1 || n == 0) return 1;
+    else {
+        return n * factorial(n-1);
+    }
+}
+
+int main() {
+    int n;
+    cin >> n;
+    int ans = factorial(n);
+    cout << ans << endl;
+    return 0;
+}
