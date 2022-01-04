585d50c450a00ac6bf3187d4a974b1b38422ded5
Sri Harsha Nadimpalli <harsha.sri@investwellonline.com>
2022-01-04 06:50:19 +0530

    Add missing returns Signed-off-by: Sri Harsha Nadimpalli <harsha.sri@investwellonline.com>

--- codingMinutes/backtracking/permutationsOfString.cpp
+++ codingMinutes/backtracking/permutationsOfString.cpp
@@ -4,11 +4,13 @@ using namespace std;
 void permute(string &s, int start, int end) {
     if(start == end) {
         cout << s << endl;
+        return;
     } else {
         for(int i = start;i<=end; i++) {
             swap(s[start],s[i]);
             permute(s,start+1,end);
             swap(s[i],s[start]);
+            return;
         }
     }
 }
