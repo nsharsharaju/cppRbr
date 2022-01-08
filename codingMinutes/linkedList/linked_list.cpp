c2e93057217bef52f84c324fb97cfb6d45c62a7f
Sri Harsha Nadimpalli <harsha.sri@investwellonline.com>
2022-01-08 15:17:49 +0530

    Add initial setup for Linked List Signed-off-by: Sri Harsha Nadimpalli <harsha.sri@investwellonline.com>

---
+++ codingMinutes/linkedList/linked_list.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include "list.h"
+using namespace std;
+
+int main() {
+    List l;
+    l.push_back(1);
+    l.push_front(2);
+    l.push_back(3);
+
+    Node * head = l.begin();
+    while(head!=NULL) {
+        cout << head->getData()<<"->";
+        head = head->next;
+    }
+    cout <<"NULL"<<endl;
+    return 0;
+}
