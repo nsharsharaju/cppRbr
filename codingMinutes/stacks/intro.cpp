1d86da32ec5ff4002b8ae080e38f46c4eaa508e9
Sri Harsha Nadimpalli <harsha.sri@investwellonline.com>
2022-01-17 05:54:30 +0530

    Add stacks intro Signed-off-by: Sri Harsha Nadimpalli <harsha.sri@investwellonline.com>

---
+++ codingMinutes/stacks/intro.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    stack<string> books;
+    books.push("C++");
+    books.push("Java");
+    books.push("Python");
+    books.push("OS");
+
+    while(!books.empty()) {
+        cout << books.top() << endl;
+        books.pop();
+    }
+    return 0;
+}
