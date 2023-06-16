346f9b66a6b2a01c300f4120604efc17fb8a6835
Ramakrishnam Raju <sri.nadimpalli@incred.com>
2023-06-16 16:59:12 +0530

    Add initial setup for quick sort

---
+++ restartApril23/quickSort.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void quickSort(int* arr, int begin, int end) {
+    return;
+}
+
+int main() {
+    int arr[] = {4,2,1,9,5,8,7,6,10,3};
+    int n = sizeof(arr)/sizeof(int);
+    n = n-1;
+    quickSort(arr,0,n);
+    return 0;
+}
