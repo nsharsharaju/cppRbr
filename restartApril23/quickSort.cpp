30ef8bfc60b361d2d2c2e3cd8d3b9893f5fc1e17
Ramakrishnam Raju <sri.nadimpalli@incred.com>
2023-07-27 18:50:40 +0530

    Add quick sort

--- restartApril23/quickSort.cpp
+++ restartApril23/quickSort.cpp
@@ -1,7 +1,26 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+int partition(int* arr, int begin, int end) {
+    int pv = arr[end];
+    int p = begin-1;
+    int i = begin;
+    while(i<end) {
+        if(arr[i]<pv) {
+            p++;
+            swap(arr[i],arr[p]);
+        }
+        i++;
+    }
+    swap(arr[p+1],arr[end]);
+    return p+1;
+}
+
 void quickSort(int* arr, int begin, int end) {
+    if(begin>=end) return;
+    int p = partition(arr,begin,end);
+    quickSort(arr,begin,p-1);
+    quickSort(arr,p+1,end);
     return;
 }
 
@@ -10,5 +29,9 @@ int main() {
     int n = sizeof(arr)/sizeof(int);
     n = n-1;
     quickSort(arr,0,n);
+    for(auto ele:arr) {
+        cout << ele << " ";
+    }
+    cout << endl;
     return 0;
 }
